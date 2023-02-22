8f4c5b0c9416669a8850f8fc48d68fae1f5da553
Michael Kupferstein <mkupfers@mail.yu.edu>
2023-02-22 17:01:50 -0500

    Version 3.1 Everything works, put method could prob be faster, named correctly

--- DataStructures/project/stage1/src/main/java/edu/yu/cs/com1320/project/impl/HashTableImpl.java
+++ DataStructures/project/stage1/src/main/java/edu/yu/cs/com1320/project/impl/HashTableImpl.java
@@ -1,20 +1,15 @@
 package edu.yu.cs.com1320.project.impl;
 import edu.yu.cs.com1320.project.HashTable;
-public class HashTableImpl<Key,Value> implements HashTable<Key,Value>{
-    private class Entry<Key, Value>{
-        Key key;
-        Value value;
-        Entry(Key k, Value v){
-            if(k == null){
-                throw new IllegalArgumentException();
-            }
-            key = k;
-            value = v;
-        }
-    }
-    private Entry<?,?>[][] table;
+
+public class HashTableImpl<Key, Value> implements HashTable<Key,Value> {
+    private myLinkedList<Key,Value>[] table;
+
     public HashTableImpl() {
-        this.table = new Entry[5][5];
+        this.table = new myLinkedList[5];
+        for(int i = 0; i < this.table.length; i++){
+            this.table[i] = new myLinkedList<Key,Value>();
+        }
+
     }
 
     private int hashFunction(Key key){
@@ -23,79 +18,84 @@ public class HashTableImpl<Key,Value> implements HashTable<Key,Value>{
 
     @Override
     public Value get(Key k) {
-       int index = this.hashFunction(k);
-       Entry current = this.table[index][0];
-       for(int i = 0; i < this.table[index].length; i++){
-           if(this.table[index][i].key.equals(k)){
-               current = this.table[index][i];
-               break;
-           }
-       }
-       if(current != null){
-           return (Value)current.value;
-       }
-       return null;
-
-    }
-    private Entry getEntry(Key k){
-        int index = this.hashFunction(k);
-        Entry current = this.table[index][0];
-        for(int i = 0; i < this.table[index].length; i++){
-            if(this.table[index][i].key.equals(k)){
-                current = this.table[index][i];
-                break;
-            }
-            current = null;
-        }
-        return current;
+        int index = hashFunction(k);
+        return this.table[index].get(k);
     }
     @Override
-    public Value put(Key k, Value v) {
-        int index = this.hashFunction(k);
-        if(getEntry(k) != null){
-            for(int i = 0; i < this.table[index].length;i++){
-                if(this.table[index][i].key.equals(k)){
-                    Entry old = this.table[index][i];
-                    this.table[index][i] = new Entry<Key,Value>(k,v);
-                    if(old != null){
-                        checkIfsized(index);
-                        return (Value)old.value;
-                    }
-                    checkIfsized(index);
-                    return null;
+    public Value put(Key k, Value v){
+        int index = hashFunction(k);
+        if(this.table[index].get(k) == null){
+            this.table[index].add(k,v);
+            return null;
+        }else{
+            Value old = this.table[index].get(k);
+            this.table[index].remove(k);
+            this.table[index].add(k,v);
+            return old;
+        }
+
+    }
+    private class myLinkedList<Key,Value>{
+        private int size = 0;
+        Node<Key,Value> head;
+        private myLinkedList(){
+            this.head = null;
+        }
+        private void add(Key k, Value v){
+            if(head == null){
+                head = new Node<>(k,v);
+                size++;
+            }else{
+                Node<Key,Value> temp = head;
+                while(temp.next != null){
+                    temp = temp.next;
+                }
+                temp.next = new Node<Key,Value>(k,v);
+                size++;
+            }
+        }
+        private void remove(Key k){
+            if(head.k.equals(k)){
+                head = head.next;
+            }else{
+                Node<Key,Value> temp = head;
+                while(temp.next != null && !(temp.k.equals(k))){
+                    temp = temp.next;
+                }
+                if(temp.next != null){
+                    temp.next = temp.next.next;
                 }
             }
         }
-        for(int i = 0; i < this.table[index].length; i++){
-            if(this.table[index][i] == null){
-                this.table[index][i] = new Entry<Key,Value>(k,v);
-                checkIfsized(index);
+        private Value get(Key k){
+            Node<Key,Value> temp = head;
+            if(temp == null){
                 return null;
             }
+            if(temp.k.equals(k)){
+                return temp.v;
+            }
+            while(temp.next != null){
+                if(temp.k.equals(k)){
+                    return temp.v;
+                }
+                temp = temp.next;
+            }
+            //return null if it doesnt exist, becuase it went through each element and none equaled k so it ended the while loop bc its null
+            return null;
         }
-        checkIfsized(index);
-        return null;
+
+        private class Node<Key,Value>{
+            private Key k;
+            private Value v;
+            private Node<Key,Value> next;
+
+            private Node(Key k1, Value v1){
+                this.k = k1;
+                this.v = v1;
+                this.next = null;
+            }
+        }
     }
 
-    private void checkIfsized(int index) {
-        boolean space = false;
-        for(int i = 0; i < this.table[index].length; i++){
-           if(this.table[index][i] == null){
-             space = true;
-             break;
-           }
-       }
-       if(!space){
-           arrayDouble(index);
-       }
-    }
-
-    private void arrayDouble(int index){
-        Entry<?,?>[] oldRow = this.table[index];
-        Entry<?,?>[] newRow = new Entry[oldRow.length * 2];
-        for(int i = 0; i < oldRow.length; i++){
-            newRow[i] = oldRow[i];
-        }
-        this.table[index] = newRow;
-    }
 }
