547582ee72205addbd8eef1680800d1593400fc9
Michael Kupferstein <mkupfers@mail.yu.edu>
2023-02-26 17:12:13 -0500

    Version 4.0 Everything is written, and should be working, test coming soon (pre new Method in HT inter)

--- DataStructures/project/stage1/src/main/java/edu/yu/cs/com1320/project/stage1/impl/DocumentStoreImpl.java
+++ DataStructures/project/stage1/src/main/java/edu/yu/cs/com1320/project/stage1/impl/DocumentStoreImpl.java
@@ -1,8 +1,10 @@
 package edu.yu.cs.com1320.project.stage1.impl;
 import edu.yu.cs.com1320.project.impl.HashTableImpl;
 import edu.yu.cs.com1320.project.stage1.Document;
+import edu.yu.cs.com1320.project.stage1.impl.DocumentImpl;
 import edu.yu.cs.com1320.project.stage1.DocumentStore;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
@@ -24,7 +26,33 @@ public class DocumentStoreImpl implements DocumentStore{
      */
     @Override
     public int put(InputStream input, URI uri, DocumentFormat format) throws IOException {
+        if(uri == null || format == null) {
+            throw new IllegalArgumentException();
+        }
+        if(input == null){
+            if (delete(uri) == false) return 0;
+            return this.hashTable.put(uri,null).hashCode();
 
+        }
+        byte[] bytes = input.readAllBytes();
+        input.close();
+        if(format.equals(DocumentFormat.BINARY)){
+            DocumentImpl temp = new DocumentImpl(uri,bytes);
+            DocumentImpl v = this.hashTable.put(uri,temp);
+            if(v == null){
+                return 0;
+            }else{
+                return v.hashCode();
+            }
+        }else if(format.equals(DocumentFormat.TXT)){
+            DocumentImpl temp = new DocumentImpl(uri, new String(bytes));
+            DocumentImpl v = this.hashTable.put(uri,temp);
+            if(v == null){
+                return 0;
+            }else{
+                return v.hashCode();
+            }
+        }
         return 0;
     }
 
@@ -34,7 +62,7 @@ public class DocumentStoreImpl implements DocumentStore{
      */
     @Override
     public Document get(URI uri) {
-        return null;
+        return this.hashTable.get(uri);
     }
 
     /**
@@ -43,7 +71,8 @@ public class DocumentStoreImpl implements DocumentStore{
      */
     @Override
     public boolean delete(URI uri) {
-        return false;
+        if(this.hashTable.get(uri) == null) return false;
+        return true;
     }
 
 }
