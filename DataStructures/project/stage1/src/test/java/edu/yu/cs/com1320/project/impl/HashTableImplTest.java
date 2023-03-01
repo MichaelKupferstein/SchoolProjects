72094eff30952b416f0b89390900a3aeaaf31268
Michael Kupferstein <mkupfers@mail.yu.edu>
2023-02-28 21:53:04 -0500

    Version 4.4 added more test for more coverage, all works

--- DataStructures/project/stage1/src/test/java/edu/yu/cs/com1320/project/impl/HashTableImplTest.java
+++ DataStructures/project/stage1/src/test/java/edu/yu/cs/com1320/project/impl/HashTableImplTest.java
@@ -7,7 +7,8 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class HashTableImplTest {
-    private HashTableImpl<Integer,String> test;
+    private HashTableImpl<Integer, String> test;
+
     @BeforeEach
     void setUp() {
         this.test = new HashTableImpl<>();
@@ -17,12 +18,13 @@ public class HashTableImplTest {
     @DisplayName("Putting inputs in for the first time")
     void initialPut() {
         //initial put should return null bc there are no entries for given key
-        assertNull(this.test.put(123,"1st Test"));
-        assertNull(this.test.put(456,"2nd Test"));
-        assertNull(this.test.put(789,"3rd Test"));
-        assertNull(this.test.put(101112,"4th Test"));
-        assertNull(this.test.put(131415,"5th Test"));
+        assertNull(this.test.put(123, "1st Test"));
+        assertNull(this.test.put(456, "2nd Test"));
+        assertNull(this.test.put(789, "3rd Test"));
+        assertNull(this.test.put(101112, "4th Test"));
+        assertNull(this.test.put(131415, "5th Test"));
     }
+
     @Test
     @DisplayName("Getting the inputs that were put in originally")
     void initialGet() {
@@ -34,9 +36,10 @@ public class HashTableImplTest {
         assertEquals("5th Test", this.test.get(131415));
 
     }
+
     @Test
     @DisplayName("Replacing the original put and seeing if it returns the old value")
-    void replacingWithPut(){
+    void replacingWithPut() {
         initialPut();
         assertEquals("1st Test", this.test.put(123, "Replacing 1st Test"));
         assertEquals("2nd Test", this.test.put(456, "Replacing 2nd Test"));
@@ -44,6 +47,7 @@ public class HashTableImplTest {
         assertEquals("4th Test", this.test.put(101112, "Replacing 4th Test"));
         assertEquals("5th Test", this.test.put(131415, "Replacing 5th Test"));
     }
+
     @Test
     @DisplayName("Checking to see if the table contains the appropriate values for each key")
     void containsKey() {
@@ -55,9 +59,10 @@ public class HashTableImplTest {
         assertThrows(NullPointerException.class, () -> this.test.containsKey(null));
         assertFalse(this.test.containsKey(1234));
     }
+
     @Test
     @DisplayName("Using get after the values have been replaced to see if they return correct values")
-    void usingGetAfterValuesHaveBeenReplaced(){
+    void usingGetAfterValuesHaveBeenReplaced() {
         replacingWithPut();
         assertEquals("Replacing 1st Test", this.test.get(123));
         assertEquals("Replacing 2nd Test", this.test.get(456));
@@ -65,11 +70,12 @@ public class HashTableImplTest {
         assertEquals("Replacing 4th Test", this.test.get(101112));
         assertEquals("Replacing 5th Test", this.test.get(131415));
     }
+
     @Test
     @DisplayName("Removing an entry by putting null as a value")
-    void puttingNullAsVtoDeleteAnEntry(){
+    void puttingNullAsVtoDeleteAnEntry() {
         replacingWithPut();
-        assertEquals("Replacing 5th Test", this.test.put(131415,null));
+        assertEquals("Replacing 5th Test", this.test.put(131415, null));
         assertFalse(this.test.containsKey(131415));
         assertNull(this.test.get(131415));
     }
