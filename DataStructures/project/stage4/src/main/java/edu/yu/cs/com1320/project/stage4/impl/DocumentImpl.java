3ca110d937c3250f6888bf270777b0691111fe28
Michael Kupferstein <mkupfers@mail.yu.edu>
2023-04-21 16:31:53 -0400

    Stage4 Version 1.1 created heap and tested it. finished new methods in docStore

--- DataStructures/project/stage4/src/main/java/edu/yu/cs/com1320/project/stage4/impl/DocumentImpl.java
+++ DataStructures/project/stage4/src/main/java/edu/yu/cs/com1320/project/stage4/impl/DocumentImpl.java
@@ -10,6 +10,7 @@ public class DocumentImpl implements Document{
     private String txt;
     private byte[] binaryData;
     private Map<String,Integer> words;
+    private long timeInNano;
     public DocumentImpl(URI uri, String txt) {
         if(uri == null || txt == null){
             throw new IllegalArgumentException();
@@ -114,48 +115,20 @@ public class DocumentImpl implements Document{
      */
     @Override
     public long getLastUseTime() {
-        return 0;
+        return this.timeInNano;
     }
 
     @Override
     public void setLastUseTime(long timeInNanoseconds) {
-
+        this.timeInNano = timeInNanoseconds;
     }
 
     /**
-     * Compares this object with the specified object for order.  Returns a
-     * negative integer, zero, or a positive integer as this object is less
-     * than, equal to, or greater than the specified object.
      *
-     * <p>The implementor must ensure {@link Integer#signum
-     * signum}{@code (x.compareTo(y)) == -signum(y.compareTo(x))} for
-     * all {@code x} and {@code y}.  (This implies that {@code
-     * x.compareTo(y)} must throw an exception if and only if {@code
-     * y.compareTo(x)} throws an exception.)
-     *
-     * <p>The implementor must also ensure that the relation is transitive:
-     * {@code (x.compareTo(y) > 0 && y.compareTo(z) > 0)} implies
-     * {@code x.compareTo(z) > 0}.
-     *
-     * <p>Finally, the implementor must ensure that {@code
-     * x.compareTo(y)==0} implies that {@code signum(x.compareTo(z))
-     * == signum(y.compareTo(z))}, for all {@code z}.
-     *
-     * @param o the object to be compared.
-     * @return a negative integer, zero, or a positive integer as this object
-     * is less than, equal to, or greater than the specified object.
-     * @throws NullPointerException if the specified object is null
-     * @throws ClassCastException   if the specified object's type prevents it
-     *                              from being compared to this object.
-     * @apiNote It is strongly recommended, but <i>not</i> strictly required that
-     * {@code (x.compareTo(y)==0) == (x.equals(y))}.  Generally speaking, any
-     * class that implements the {@code Comparable} interface and violates
-     * this condition should clearly indicate this fact.  The recommended
-     * language is "Note: this class has a natural ordering that is
-     * inconsistent with equals."
      */
     @Override
     public int compareTo(Document o) {
-        return 0;
+        return Long.compare(timeInNano,o.getLastUseTime());
     }
+
 }
