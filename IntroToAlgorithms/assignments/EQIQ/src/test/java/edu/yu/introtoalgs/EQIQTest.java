2c8276984582d5a31469f4800e33a22598124259
Michael Kupferstein <116312352+MichaelKupferstein@users.noreply.github.com>
2023-11-12 12:59:58 -0500

    added tests, passed

--- IntroToAlgorithms/assignments/EQIQ/src/test/java/edu/yu/introtoalgs/EQIQTest.java
+++ IntroToAlgorithms/assignments/EQIQ/src/test/java/edu/yu/introtoalgs/EQIQTest.java
@@ -22,4 +22,48 @@ class EQIQTest {
         assertEquals(0.0, eqiq.getNumberIQQuestions(), DELTA);
         assertEquals(360, eqiq.getNumberOfSecondsSuccess(), DELTA);
     }
+
+    @Test
+    void ownTest(){
+        int totalQuestions = 2;
+        int nCandidates = 3;
+        double[] eqSuccessRate = {10.0, 20.0,15.0};
+        double[] iqSuccessRate = {40.0, 40.0,40.0};
+        int nepotismIndex = 1;
+        final double DELTA = 0.001;
+        EQIQBase eqiq = new EQIQ(totalQuestions, eqSuccessRate, iqSuccessRate, nepotismIndex);
+        assertTrue(eqiq.canNepotismSucceed());
+        assertEquals(2.0, eqiq.getNumberEQQuestions(), DELTA);
+        assertEquals(0.0, eqiq.getNumberIQQuestions(), DELTA);
+        assertEquals(120, eqiq.getNumberOfSecondsSuccess(), DELTA);
+    }
+    @Test
+    void testWithMoreQuestions(){
+        int totalQuestions = 5;
+        int nCandidates = 3;
+        double[] eqSuccessRate = {10.0, 20.0,15.0};
+        double[] iqSuccessRate = {40.0, 40.0,40.0};
+        int nepotismIndex = 1;
+        final double DELTA = 0.001;
+        EQIQBase eqiq = new EQIQ(totalQuestions, eqSuccessRate, iqSuccessRate, nepotismIndex);
+        assertTrue(eqiq.canNepotismSucceed());
+        assertEquals(5.0, eqiq.getNumberEQQuestions(), DELTA);
+        assertEquals(0.0, eqiq.getNumberIQQuestions(), DELTA);
+        assertEquals(300, eqiq.getNumberOfSecondsSuccess(), DELTA);
+    }
+
+    @Test
+    void testWithDifferntNep(){
+        int totalQuestions = 2;
+        int nCandidates = 3;
+        double[] eqSuccessRate = {10.0, 20.0,15.0,30.0};
+        double[] iqSuccessRate = {40.0, 40.0,40.0,40.0};
+        int nepotismIndex = 3;
+        final double DELTA = 0.001;
+        EQIQBase eqiq = new EQIQ(totalQuestions, eqSuccessRate, iqSuccessRate, nepotismIndex);
+        assertTrue(eqiq.canNepotismSucceed());
+        assertEquals(2.0, eqiq.getNumberEQQuestions(), DELTA);
+        assertEquals(0.0, eqiq.getNumberIQQuestions(), DELTA);
+        assertEquals(120, eqiq.getNumberOfSecondsSuccess(), DELTA);
+    }
 }
