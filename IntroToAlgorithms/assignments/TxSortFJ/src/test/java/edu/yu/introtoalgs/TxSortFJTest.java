a19f6e3bea6a53ac4f63ed1cd8629bb6d9e3e152
Michael Kupferstein <116312352+MichaelKupferstein@users.noreply.github.com>
2023-11-30 17:04:33 -0500

    added something to see how long basic sorting takes in comparison to my sort

--- IntroToAlgorithms/assignments/TxSortFJ/src/test/java/edu/yu/introtoalgs/TxSortFJTest.java
+++ IntroToAlgorithms/assignments/TxSortFJ/src/test/java/edu/yu/introtoalgs/TxSortFJTest.java
@@ -33,28 +33,51 @@ class TxSortFJTest {
         String testName = testInfo.getTestMethod().get().getName();
         logger.info("Test {} took {} ms", testName, duration);
     }
+
     @Test
-    void TestFromDoc(){
-        final int nAccounts = 2;
-        final int nTxs = 5;
+    void TestFromDocs(){
+        TestWithInputs(2, 5);
+    }
+    @Test
+    void TestWith1000(){
+        TestWithInputs(1000, 1000);
+    }
+    @Test
+    void TestWith10000(){
+        TestWithInputs(10000, 10000);
+    }
+    @Test
+    void TestWith9_000_000Txs(){
+        TestWithInputs(1_000_000, 9_000_000);
+    }
+    void TestWithInputs(int nAccount, int nTxs){
         final List<TxBase> txs = new ArrayList<>();
-        final Account[] accounts = new Account[nAccounts];
-        for (int i = 0; i < nAccounts; ++i) {
+        final Account[] accounts = new Account[nAccount];
+        for (int i = 0; i < nAccount; ++i) {
             accounts[i] = new Account();
         }
-        logger.info("Created {} accounts", nAccounts);
+        logger.info("Created {} accounts", nAccount);
         for(int i = 0; i < nTxs; i++) {
             //being silly here: no point in making this look more real
-            final Account account1 = accounts[random.nextInt(0, nAccounts)];
-            final Account account2 = accounts[random.nextInt(0, nAccounts)];
+            final Account account1 = accounts[random.nextInt(0, nAccount)];
+            final Account account2 = accounts[random.nextInt(0, nAccount)];
             txs.add(new Tx(account1, account2, 1));
         }
         Collections.shuffle(txs);
         logger.info("Created {} Txs", txs.size());
 
+        List<TxBase> copyOfTxs = new ArrayList<>(txs);
+        final long beforeNaive = System.currentTimeMillis();
+        Collections.sort(copyOfTxs);
+        final long afterNaive = System.currentTimeMillis();
+        logger.info("Basic sorting took {} ms", afterNaive - beforeNaive);
+
         try {
             final TxSortFJBase txSortFJ = new TxSortFJ(txs);
+            final long before = System.currentTimeMillis();
             final TxBase[] sortedTxs = txSortFJ.sort();
+            final long after = System.currentTimeMillis();
+            logger.info("Sorting took {} ms", after - before);
             final boolean isSorted = isSorted(sortedTxs);
             logger.info("isSorted: {}", isSorted);
         }catch (Exception e) {
@@ -64,7 +87,6 @@ class TxSortFJTest {
         }finally {
             Configurator.shutdown(null);
         }
-
     }
 
     private boolean isSorted(TxBase[] sortedTxs) {
