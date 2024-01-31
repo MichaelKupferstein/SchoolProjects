4650d1af63c406ee74f63f0ee97ca01c7718a633
Michael Kupferstein <116312352+MichaelKupferstein@users.noreply.github.com>
2024-01-31 17:04:10 -0500

    Made a simple random graph generator and test

---
+++ DesignAndAnalysis/assignments/ThereAndBackAgain/src/test/java/edu/yu/da/tests/RandomGraphGeneratorTest.java
@@ -0,0 +1,38 @@
+package edu.yu.da.tests;
+
+import edu.yu.da.Dijkstra;
+import edu.yu.da.EdgeWeightedGraph;
+
+import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+import java.util.ArrayList;
+
+
+class RandomGraphGeneratorTest {
+    @Test
+    void generate(){
+        EdgeWeightedGraph graph = new RandomGraphGenerator(10).generate();
+        System.out.println(graph.toString());
+    }
+
+    @Test
+    void TestWithDijkstra(){
+        EdgeWeightedGraph graph = new RandomGraphGenerator(10).generate();
+        //new GraphVisualizer(graph);
+
+        //GraphVisualizer graphVisualizer = new GraphVisualizer(graph);
+        String startVertex = graph.vertices().iterator().next();
+        Dijkstra dijkstra = new Dijkstra(graph, startVertex);
+        System.out.printf("Start Vertex: %s\n", startVertex);
+        for(String vertex : graph.vertices()){
+            System.out.printf("Vertex: %s, Distance: %f, Path: %s\n", vertex, dijkstra.distTo(vertex), dijkstra.pathTo(vertex));
+        }
+
+        try{
+            Thread.sleep(10000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+    }
+}
