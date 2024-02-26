f0c1e8e1e807b306cbc22fbf0f535d08f5c9ff4e
Michael Kupferstein <116312352+MichaelKupferstein@users.noreply.github.com>
2024-02-26 17:01:23 -0500

    added a "check" method

--- DesignAndAnalysis/assignments/PickAYeshiva/src/main/java/edu/yu/da/Yeshiva.java
+++ DesignAndAnalysis/assignments/PickAYeshiva/src/main/java/edu/yu/da/Yeshiva.java
@@ -20,6 +20,25 @@ public class Yeshiva {
         return cookingRanking;
     }
 
+    /**
+     * This method compares the current Yeshiva object with another Yeshiva object based on their cookingRanking and facultyRatioRanking.
+     *
+     * @param other The other Yeshiva object to be compared with.
+     * @return returns 1 if the current Yeshiva object has higher cookingRanking and facultyRatioRanking than the other Yeshiva object,
+     * indicating that the other Yeshiva object should be deleted.
+     * It returns -1 if the other Yeshiva object has higher cookingRanking and facultyRatioRanking than the current Yeshiva object,
+     * indicating that the current Yeshiva object should be deleted.
+     * It returns 0 if neither of the Yeshiva objects should be deleted.
+     */
+    public int check(Yeshiva other){
+        //if(this.cookingRanking == other.cookingRanking && this.facultyRatioRanking == other.facultyRatioRanking) return 0; //cant be bc no duplicates
+        if(this.cookingRanking > other.cookingRanking && this.facultyRatioRanking > other.facultyRatioRanking)
+            return 1; // this is greater in both aspects, so other should be deleted
+        if(this.cookingRanking < other.cookingRanking && this.facultyRatioRanking < other.facultyRatioRanking)
+            return -1; //other is greater in both aspects, so this should be deleted
+        return 0; // neither should be deleted
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
