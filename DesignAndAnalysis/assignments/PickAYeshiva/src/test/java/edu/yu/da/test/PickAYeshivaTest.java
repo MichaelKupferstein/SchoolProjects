b1162a6bbaae0fdf47562e229701f6aefc311253
Michael Kupferstein <116312352+MichaelKupferstein@users.noreply.github.com>
2024-02-26 18:08:05 -0500

    found bug, need to work it out

--- DesignAndAnalysis/assignments/PickAYeshiva/src/test/java/edu/yu/da/test/PickAYeshivaTest.java
+++ DesignAndAnalysis/assignments/PickAYeshiva/src/test/java/edu/yu/da/test/PickAYeshivaTest.java
@@ -15,11 +15,55 @@ class PickAYeshivaTest {
         double[] facultyRatioRankings = {0,1,2};
         double[] cookingRankings = {3,2,1};
         PickAYeshivaBase pickAYeshiva = new PickAYeshiva(facultyRatioRankings, cookingRankings);
-        //print out the arrays
+        assertArrayEquals(facultyRatioRankings, pickAYeshiva.getFacultyRatioRankings());
+        assertArrayEquals(cookingRankings, pickAYeshiva.getCookingRankings());
+    }
+
+    @Test
+    void test1(){
+        double[] facultyRatioRankings = {0,1,2,3,4,5,6,7,8,9};
+        double[] cookingRankings = {9,8,7,6,5,4,3,2,1,0};
+        PickAYeshivaBase pickAYeshiva = new PickAYeshiva(facultyRatioRankings, cookingRankings);
+        assertArrayEquals(facultyRatioRankings, pickAYeshiva.getFacultyRatioRankings());
+        assertArrayEquals(cookingRankings, pickAYeshiva.getCookingRankings());
+    }
+
+    @Test
+    void testSimpleItDelets(){
+        double[] facultyRatioRankings = {10,5};
+        double[] cookingRankings = {10,5};
+        PickAYeshivaBase pickAYeshiva = new PickAYeshiva(facultyRatioRankings, cookingRankings);
+        assertArrayEquals(new double[]{10}, pickAYeshiva.getFacultyRatioRankings());
+        assertArrayEquals(new double[]{10}, pickAYeshiva.getCookingRankings());
+    }
+    @Test
+    void testSimpleItDelets2(){
+        double[] facultyRatioRankings = {10,4,5};
+        double[] cookingRankings = {10,11,5};
+        PickAYeshivaBase pickAYeshiva = new PickAYeshiva(facultyRatioRankings, cookingRankings);
+        assertArrayEquals(new double[]{10,4}, pickAYeshiva.getFacultyRatioRankings());
+        assertArrayEquals(new double[]{10,11}, pickAYeshiva.getCookingRankings());
+    }
+    @Test
+    void testSimpleItDelets3(){
+        double[] facultyRatioRankings = {10,5,4};
+        double[] cookingRankings = {10,5,11};
+        PickAYeshivaBase pickAYeshiva = new PickAYeshiva(facultyRatioRankings, cookingRankings);
+        assertArrayEquals(new double[]{10,4}, pickAYeshiva.getFacultyRatioRankings());
+        assertArrayEquals(new double[]{10,11}, pickAYeshiva.getCookingRankings());
+    }
+
+
+
+    @Test //TODO: fix this
+    void testThatItDeletes(){ //FALES
+        double[] facultyRatioRankings = {10,2,3,2,4,5};
+             double[] cookingRankings = {10,1,0,6,11,5};
+        PickAYeshivaBase pickAYeshiva = new PickAYeshiva(facultyRatioRankings, cookingRankings);
         System.out.println(Arrays.toString(pickAYeshiva.getFacultyRatioRankings()));
         System.out.println(Arrays.toString(pickAYeshiva.getCookingRankings()));
-        assertArrayEquals(new double[]{0,1,2}, pickAYeshiva.getFacultyRatioRankings());
-        assertArrayEquals(new double[]{3,2,1}, pickAYeshiva.getCookingRankings());
+        assertArrayEquals(new double[]{10,4}, pickAYeshiva.getFacultyRatioRankings());
+        assertArrayEquals(new double[]{10,11}, pickAYeshiva.getCookingRankings());
     }
 
 }
