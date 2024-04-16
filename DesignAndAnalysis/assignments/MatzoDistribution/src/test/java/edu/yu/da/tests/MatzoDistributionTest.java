937930f94cd246c12e29a42a0e7b5cdf94c40bb4
Michael Kupferstein <116312352+MichaelKupferstein@users.noreply.github.com>
2024-04-16 13:49:15 -0400

    added throws tests

--- DesignAndAnalysis/assignments/MatzoDistribution/src/test/java/edu/yu/da/tests/MatzoDistributionTest.java
+++ DesignAndAnalysis/assignments/MatzoDistribution/src/test/java/edu/yu/da/tests/MatzoDistributionTest.java
@@ -4,7 +4,40 @@ import edu.yu.da.MatzoDistribution;
 import edu.yu.da.MatzoDistributionBase;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MatzoDistributionTest {
+
+    @Test
+    void testConstuctorThrows(){
+        assertThrows(IllegalArgumentException.class, () -> new MatzoDistribution("", 4, "t"));//sourceWarehouse cannot be empty
+        assertThrows(IllegalArgumentException.class, () -> new MatzoDistribution("s", 0, "t"));//sourceConstraint must be positive
+        assertThrows(IllegalArgumentException.class, () -> new MatzoDistribution("s", 4, ""));//destinationWarehouse cannot be empty
+        assertThrows(IllegalArgumentException.class, () -> new MatzoDistribution("s", 4, "s"));//sourceWarehouse and destinationWarehouse cannot be equal
+
+    }
+
+    @Test
+    void testAddWarehouseThrows(){
+        MatzoDistributionBase md = new MatzoDistribution("s", 4, "t");
+        md.addWarehouse("A", 10);
+        assertThrows(IllegalArgumentException.class, () -> md.addWarehouse("", 10));//warehouseId cannot be empty
+        assertThrows(IllegalArgumentException.class, () -> md.addWarehouse("B", 0));//constraint must be positive
+        assertThrows(IllegalArgumentException.class, () -> md.addWarehouse("A", 10));//warehouseId cannot be added twice
+    }
+
+    @Test
+    void testRoadExistsThrows(){
+        MatzoDistributionBase md = new MatzoDistribution("s", 4, "t");
+        md.addWarehouse("A", 10);
+        md.roadExists("s", "A", 10);
+        assertThrows(IllegalArgumentException.class, () -> md.roadExists("", "A", 10));//w1 cannot be empty
+        assertThrows(IllegalArgumentException.class, () -> md.roadExists("A", "", 10));//w2 cannot be empty
+        assertThrows(IllegalArgumentException.class, () -> md.roadExists("A", "t", 0));//constraint must be positive
+        assertThrows(IllegalArgumentException.class, () -> md.roadExists("A", "A", 10));//w1 and w2 cannot be equal
+        assertThrows(IllegalArgumentException.class, () -> md.roadExists("A", "B", 10));//w1 and w2 must be added to the network
+        assertThrows(IllegalArgumentException.class, () -> md.roadExists("B", "A", 10));//w1 and w2 must be added to the network
+    }
 
     @Test
     void testFromDoc(){
@@ -12,6 +45,6 @@ public class MatzoDistributionTest {
         md.addWarehouse("A", 10);
         md.roadExists("s", "A", 10);
         md.roadExists("A", "t", 10);
-        System.out.println(md.max());
+        assertEquals(4, md.max());
     }
 }
