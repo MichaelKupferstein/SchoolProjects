28b2bddd3e1cfe88a667c612c9ee615a4876c0fb
Michael Kupferstein <116312352+MichaelKupferstein@users.noreply.github.com>
2024-11-24 19:21:28 -0500

    initial GatewayServer constructor

--- com3800/stage4/src/main/java/edu/yu/cs/com3800/stage4/GatewayServer.java
+++ com3800/stage4/src/main/java/edu/yu/cs/com3800/stage4/GatewayServer.java
@@ -1,4 +1,29 @@
 package edu.yu.cs.com3800.stage4;
 
-public class GatewayServer {
+import com.sun.net.httpserver.HttpServer;
+import edu.yu.cs.com3800.LoggingServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.logging.Logger;
+
+public class GatewayServer implements LoggingServer {
+
+    private HttpServer httpServer;
+    private GatewayPeerServerImpl peerServer;
+    private int httpPort;
+    private ConcurrentHashMap<Integer, byte[]> cache;
+    private Logger logger;
+
+    public GatewayServer(int httpPort, int peerPort, long peerEpoch, Long serverID,
+                         ConcurrentHashMap<Long, InetSocketAddress> peerIDtoAdress, int numberOfObservers) throws IOException{
+        this.httpPort = httpPort;
+        this.httpServer = HttpServer.create(new InetSocketAddress(httpPort),0); //create an HTTPServer on...
+        this.cache = new ConcurrentHashMap<>();
+        this.logger = initializeLogging(GatewayServer.class.getCanonicalName() + "-on-port-" + this.httpPort);
+        this.peerServer = new GatewayPeerServerImpl(peerPort, peerEpoch,serverID,peerIDtoAdress,numberOfObservers);
+
+    }
+
 }
