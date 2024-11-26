83596ea2065ddf37d07b91464c33acb79c538c6f
Michael Kupferstein <116312352+MichaelKupferstein@users.noreply.github.com>
2024-11-25 23:12:27 -0500

    updated GatewayServerImpl again

--- com3800/stage4/src/main/java/edu/yu/cs/com3800/stage4/GatewayPeerServerImpl.java
+++ com3800/stage4/src/main/java/edu/yu/cs/com3800/stage4/GatewayPeerServerImpl.java
@@ -3,12 +3,17 @@ package edu.yu.cs.com3800.stage4;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.Map;
+import java.util.logging.Logger;
 
 public class GatewayPeerServerImpl extends PeerServerImpl{
+
+    private final Logger logger;
 
     public GatewayPeerServerImpl(int udpPort, long peerEpoch, Long id, Map<Long, InetSocketAddress> peerIDtoAddress, int numOfObservers) throws IOException {
         super(udpPort, peerEpoch, id, peerIDtoAddress,id, numOfObservers);
         setPeerState(ServerState.OBSERVER);
+        this.logger = initializeLogging(GatewayPeerServerImpl.class.getCanonicalName() + "-on-port-" + udpPort);
+        logger.info("GatewayPeerServer initialized on port " + udpPort);
     }
 
     @Override
