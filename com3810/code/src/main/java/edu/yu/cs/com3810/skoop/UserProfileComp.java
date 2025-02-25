ce67bf35e3d096e8890bedbc558038b81a6eb6ba
Michael Kupferstein <116312352+MichaelKupferstein@users.noreply.github.com>
2025-02-24 19:24:58 -0500

    created some methods for the UserProfileComp

--- com3810/code/src/main/java/edu/yu/cs/com3810/skoop/UserProfileComp.java
+++ com3810/code/src/main/java/edu/yu/cs/com3810/skoop/UserProfileComp.java
@@ -4,4 +4,51 @@ public class UserProfileComp {
 
     //manages the user's profile
     //can access their data, manage rides
+
+    public void createUserProfile() {
+        //create a new user profile
+    }
+
+    public void updateUserProfile() {
+        //update the user's profile
+    }
+
+    public void deleteUserProfile() {
+        //delete the user's profile
+    }
+
+    public void viewUserProfile() {
+        //view the user's profile
+    }
+
+    public void manageRides() {
+        //manage the user's rides
+    }
+
+    public void viewRides() {
+        //view the user's rides
+    }
+
+    public void editRides() {
+        //edit the user's rides
+    }
+
+    public void deleteRides() {
+        //delete the user's rides
+    }
+
+    public void createRides() {
+        //create a new ride
+    }
+
+    public void viewRideDetails() {
+        //view the details of a ride
+    }
+
+    public void viewRideHistory() {
+        //view the user's ride history
+    }
+
+
+
 }
