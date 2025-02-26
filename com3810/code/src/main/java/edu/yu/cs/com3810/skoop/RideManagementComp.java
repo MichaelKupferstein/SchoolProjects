ad171df9e007a68adaae4b4c3ee476f221ffe313
Michael Kupferstein <116312352+MichaelKupferstein@users.noreply.github.com>
2025-02-25 22:49:24 -0500

    improved ride managment

--- com3810/code/src/main/java/edu/yu/cs/com3810/skoop/RideManagementComp.java
+++
@@ -1,7 +0,0 @@
-package edu.yu.cs.com3810.skoop;
-
-public class RideManagementComp {
-    //responsible for creating, updating, searching, editing, and deleting rides
-    //reads/writes to the database
-    //show real-time updates to the user relevant to the ride
-}
