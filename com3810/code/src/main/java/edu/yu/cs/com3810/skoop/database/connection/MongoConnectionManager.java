6ef41b48c21d58f47fcff688f6110498451df843
Michael Kupferstein <116312352+MichaelKupferstein@users.noreply.github.com>
2025-04-08 11:35:23 -0400

    Delete com3810 directory

--- com3810/code/src/main/java/edu/yu/cs/com3810/skoop/database/connection/MongoConnectionManager.java
+++
@@ -1,9 +0,0 @@
-package edu.yu.cs.com3810.skoop.database.connection;
-
-/**
- * Database configuration and connection management for MongoDB (Ride data).
- */
-public class MongoConnectionManager {
-
-    //implementation needed
-}
